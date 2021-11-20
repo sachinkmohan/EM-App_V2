7ad688c8d1e38b14eea5ed442deb044b50ffad7b
Sachin <sachinkm308@gmail.com>
2021-11-20 12:52:36 +0100

    Screen Optimizations in Navigator, Including Section-6: 126

--- navigation/ItemsNavigator.js
+++ navigation/ItemsNavigator.js
@@ -1,14 +1,31 @@
 //import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryItemsScreen from '../screens/CategoryItemsScreen';
 
+import Colors from '../constants/Colors';
+
 const ItemsNavigator = createStackNavigator({
-    Categories: CategoriesScreen,
+    Categories: {
+        screen: CategoriesScreen, 
+        navigationOptions: { 
+          headerTitle: 'Categories Aanu',
+    }},
     CategoryItems: {
-        screen: CategoryItemsScreen
+        screen: CategoryItemsScreen,
+        navigationOptions: {
+        }
+    },
+
+}, {
+    defaultNavigationOptions: {
+          headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+          },
+          headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor // white tint not working on the header
     }
 });
 
