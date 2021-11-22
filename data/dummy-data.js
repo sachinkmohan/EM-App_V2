759e457b5a8220fb11556b81c8a5d9e85180ccf6
Sachin <sachinkm308@gmail.com>
2021-11-22 22:21:19 +0100

    Adding Data to Category Items Screen

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,14 +1,114 @@
-import Category from '../models/category';
+import Category from "../models/category";
 
 export const CATEGORIES = [
-    new Category('c1', 'Introduction', '#f5428d'),
-    new Category('c2', 'Most confused Letters', '#f54242'),
-    new Category('c3', 'Most used Grammar', '#f5a442'),
-    new Category('c4', 'Past & Future', '#f5d142'),
-    new Category('c5', 'Resources', '#368dff'),
-    new Category('c6', 'TBD1', '#41d95d'),
-    new Category('c7', 'TBD2', '#9eecff'),
-    new Category('c8', 'TBD3', '#b9ffb0'),
-    new Category('c9', 'TBD4', '#ffc7ff'),
-    new Category('c10', 'TBD5', '#47fced')
-  ];
+	new Category(
+		"c1",
+		"Introduction",
+		"#f5428d",
+		[
+			"4 Intro",
+			"1 Tablespoon of Olive Oil",
+			"1 Onion",
+			"250g Spaghetti",
+			"Spices",
+			"Cheese (optional)",
+		],
+		[
+			"Cut the tomatoes and the onion into small pieces.",
+			"Boil some water - add salt to it once it boils.",
+			"Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.",
+			"In the meantime, heaten up some olive oil and add the cut onion.",
+			"After 2 minutes, add the tomato pieces, salt, pepper and your other spices.",
+			"The sauce will be done once the spaghetti are.",
+			"Feel free to add some cheese on top of the finished dish.",
+		]
+	),
+	new Category(
+		"c2",
+		"Most confused Letters",
+		"#f54242",
+		[
+			"4 MCL",
+			"1 Tablespoon of Olive Oil",
+			"1 Onion",
+			"250g Spaghetti",
+			"Spices",
+			"Cheese (optional)",
+		],
+		[
+			"Cut the tomatoes and the onion into small pieces.",
+			"Boil some water - add salt to it once it boils.",
+			"Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.",
+			"In the meantime, heaten up some olive oil and add the cut onion.",
+			"After 2 minutes, add the tomato pieces, salt, pepper and your other spices.",
+			"The sauce will be done once the spaghetti are.",
+			"Feel free to add some cheese on top of the finished dish.",
+		]
+	),
+	new Category(
+		"c3",
+		"Most used Grammar",
+		"#f5a442",
+		[
+			"4 MUG",
+			"1 Tablespoon of Olive Oil",
+			"1 Onion",
+			"250g Spaghetti",
+			"Spices",
+			"Cheese (optional)",
+		],
+		[
+			"Cut the tomatoes and the onion into small pieces.",
+			"Boil some water - add salt to it once it boils.",
+			"Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.",
+			"In the meantime, heaten up some olive oil and add the cut onion.",
+			"After 2 minutes, add the tomato pieces, salt, pepper and your other spices.",
+			"The sauce will be done once the spaghetti are.",
+			"Feel free to add some cheese on top of the finished dish.",
+		]
+	),
+	new Category(
+		"c4",
+		"Past & Future",
+		"#f5d142",
+		[
+			"4 Past & Future",
+			"1 Tablespoon of Olive Oil",
+			"1 Onion",
+			"250g Spaghetti",
+			"Spices",
+			"Cheese (optional)",
+		],
+		[
+			"Cut the tomatoes and the onion into small pieces.",
+			"Boil some water - add salt to it once it boils.",
+			"Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.",
+			"In the meantime, heaten up some olive oil and add the cut onion.",
+			"After 2 minutes, add the tomato pieces, salt, pepper and your other spices.",
+			"The sauce will be done once the spaghetti are.",
+			"Feel free to add some cheese on top of the finished dish.",
+		]
+	),
+	new Category(
+		"c5",
+		"Resources",
+		"#368dff",
+		[
+			"4 Resources",
+			"1 Tablespoon of Olive Oil",
+			"1 Onion",
+			"250g Spaghetti",
+			"Spices",
+			"Cheese (optional)",
+		],
+		[
+			"Cut the tomatoes and the onion into small pieces.",
+			"Boil some water - add salt to it once it boils.",
+			"Put the spaghetti into the boiling water - they should be done in about 10 to 12 minutes.",
+			"In the meantime, heaten up some olive oil and add the cut onion.",
+			"After 2 minutes, add the tomato pieces, salt, pepper and your other spices.",
+			"The sauce will be done once the spaghetti are.",
+			"Feel free to add some cheese on top of the finished dish.",
+		]
+	),
+];
--- screens/CategoryItemsScreen.js
+++ screens/CategoryItemsScreen.js
@@ -1,37 +1,53 @@
+import React from "react";
+import { ScrollView, View, Text, Image, StyleSheet } from "react-native";
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { CATEGORIES } from "../data/dummy-data";
 
-import { CATEGORIES } from '../data/dummy-data';
+const CategoryItemsScreen = (props) => {
+	const catId = props.navigation.getParam("categoryId");
 
-const CategoryItemsScreen = props => {
-  const catId = props.navigation.getParam('categoryId');
+	const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
-
-  return (
-    <View style={styles.screen}>
-      <Text>The Categories Items Screen!</Text>
-      <Text>{selectedCategory.title}</Text>
-    </View>
-  );
+	return (
+		<ScrollView>
+			{/* <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} /> */}
+			<Text style={styles.title}>Content</Text>
+			{selectedCategory.firstContent.map((firstcontent) => (
+				<Text>{firstcontent}</Text>
+			))}
+			<Text style={styles.title}>Sub steps</Text>
+			{selectedCategory.secondContent.map((secondcontent) => (
+				<Text>{secondcontent}</Text>
+			))}
+			<View style={styles.screen}>
+				{/* <Text>The Categories Items Screen!</Text>
+				<Text>{selectedCategory.title}</Text>
+				<Text>{selectedCategory.id}</Text> */}
+			</View>
+		</ScrollView>
+	);
 };
 
-CategoryItemsScreen.navigationOptions = navigationData => {
-  const catId = navigationData.navigation.getParam('categoryId');
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+CategoryItemsScreen.navigationOptions = (navigationData) => {
+	const catId = navigationData.navigation.getParam("categoryId");
+	const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
-  return {
-      headerTitle: selectedCategory.title
-  };
+	return {
+		headerTitle: selectedCategory.title,
+	};
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+	screen: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	title: {
+		fontFamily: "open-sans-bold",
+		fontSize: 22,
+		textAlign: "center",
+	},
 });
 
 export default CategoryItemsScreen;
