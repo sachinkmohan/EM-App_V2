519a4fe42ab9a2b4a1105db9a3f483112eb29ed3
Sachin <sachinkm308@gmail.com>
2021-11-30 11:50:54 +0100

    updated icon and splash screens

--- screens/CategoryItemsScreen.js
+++ screens/CategoryItemsScreen.js
@@ -7,64 +7,76 @@ import {
 	Button,
 	StyleSheet,
 } from "react-native";
-import * as Notifications from "expo-notifications";
-import * as Permissions from "expo-permissions";
+// import * as Notifications from "expo-notifications";
+// import * as Permissions from "expo-permissions";
 
-Notifications.setNotificationHandler({
-	handleNotification: async () => {
-		return {
-			shouldShowAlert: true,
-		};
-	},
-});
+// Notifications.setNotificationHandler({
+// 	handleNotification: async () => {
+// 		return {
+// 			shouldShowAlert: true,
+// 		};
+// 	},
+// });
 
 import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryItemsScreen = (props) => {
 	const catId = props.navigation.getParam("categoryId");
 
-	useEffect(() => {
-		Permissions.getAsync(Permissions.NOTIFICATIONS)
-			.then((statusObj) => {
-				if (statusObj.status !== "granted") {
-					return Permissions.askAsync(Permissions.NOTIFICATIONS);
-				}
-				return statusObj;
-			})
-			.then((statusObj) => {
-				if (statusObj.status !== "granted") {
-					return;
-				}
-			});
-	}, []);
+	// useEffect(() => {
+	// 	Permissions.getAsync(Permissions.NOTIFICATIONS)
+	// 		.then((statusObj) => {
+	// 			if (statusObj.status !== "granted") {
+	// 				return Permissions.askAsync(Permissions.NOTIFICATIONS);
+	// 			}
+	// 			return statusObj;
+	// 		})
+	// 		.then((statusObj) => {
+	// 			if (statusObj.status !== "granted") {
+	// 				throw new Error("Permission not granted");
+	// 			}
+	// 		})
+	// 		.then(() => {
+	// 			console.log("Getting token");
+	// 			return Notifications.getExpoPushTokenAsync();
+	// 		})
+	// 		.then((data) => {
+	// 			console.log(data);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log(err);
+	// 			return null;
+	// 		});
+	// }, []);
 
-	useEffect(() => {
-		const backgroundSubscription =
-			Notifications.addNotificationResponseReceivedListener((response) => {
-				console.log(response);
-			});
+	// useEffect(() => {
+	// 	const backgroundSubscription =
+	// 		Notifications.addNotificationResponseReceivedListener((response) => {
+	// 			console.log(response);
+	// 		});
 
-		const foregroundSubscription =
-			Notifications.addNotificationReceivedListener((notitification) => {
-				console.log(notitification);
-			});
+	// 	const foregroundSubscription =
+	// 		Notifications.addNotificationReceivedListener((notitification) => {
+	// 			console.log(notitification);
+	// 		});
 
-		return () => {
-			backgroundSubscription.remove();
-			foregroundSubscription.remove();
-		};
-	}, []);
+	// 	return () => {
+	// 		backgroundSubscription.remove();
+	// 		foregroundSubscription.remove();
+	// 	};
+	// }, []);
+	// Commenting push notifications for now.
 
 	const triggerNotificationHandler = () => {
-		Notifications.scheduleNotificationAsync({
-			content: {
-				title: "My first notification",
-				body: "This is the first one you are receiving",
-			},
-			trigger: {
-				seconds: 10,
-			},
-		});
+		// Notifications.scheduleNotificationAsync({
+		// 	content: {
+		// 		title: "My first notification",
+		// 		body: "This is the first one you are receiving",
+		// 	},
+		// 	trigger: {
+		// 		seconds: 10,
+		// 	},
+		// }); // Commenting as we are not using local notifications anymore.
 	};
 
 	const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
@@ -84,11 +96,11 @@ const CategoryItemsScreen = (props) => {
 				{/* <Text>The Categories Items Screen!</Text>
 				<Text>{selectedCategory.title}</Text>
 				<Text>{selectedCategory.id}</Text> */}
-				<Button
+				{/* <Button
 					title="Trigger Notification"
 					onPress={triggerNotificationHandler}
-				/>
-				{/* to test the local notification  */}
+				/> */}
+				{/* to test the local notification - for now disabling it */}
 			</View>
 		</ScrollView>
 	);
